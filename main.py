6c077212fb46b998c5c45bcef1df3806ffc0a1d7
Guilherme Ferreira <guilherme.ferreira1ss@gmail.com>
2025-12-29 16:29:21 -0300

    update auth

--- main.py
+++ main.py
@@ -87,13 +87,14 @@ def gerar_tweet():
     return tweet
 
 def postar_tweet(tweet):
-    client = tweepy.Client(
-        consumer_key=os.environ["API_KEY"],
-        consumer_secret=os.environ["API_SECRET"],
-        access_token=os.environ["ACCESS_TOKEN"],
-        access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+    auth = tweepy.OAuth1UserHandler(
+        os.environ["API_KEY"],
+        os.environ["API_SECRET"],
+        os.environ["ACCESS_TOKEN"],
+        os.environ["ACCESS_TOKEN_SECRET"]
     )
-    client.create_tweet(text=tweet)
+    api = tweepy.API(auth)
+    api.update_status(tweet)
     print("Tweet postado com sucesso.")
 
 if __name__ == "__main__":
